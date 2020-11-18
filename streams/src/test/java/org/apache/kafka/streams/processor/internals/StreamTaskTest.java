ecb1de11e6307610a3a6c8dcb32f220c90e199f9
John Roesler <vvcephei@apache.org>
2020-11-18 13:30:37 -0600

    KAFKA-10866: Add fetched metadata to ConsumerRecords
    Exposed the fetched metadata via the ConsumerRecords
    object as described in KIP-695

--- streams/src/test/java/org/apache/kafka/streams/processor/internals/StreamTaskTest.java
+++ streams/src/test/java/org/apache/kafka/streams/processor/internals/StreamTaskTest.java
@@ -1582,7 +1582,7 @@ public class StreamTaskTest {
         task.postCommit(true);
         EasyMock.verify(stateManager);
     }
-    
+
     @Test
     public void shouldNotCheckpointForSuspendedRunningTaskWithSmallProgress() {
         EasyMock.expect(stateManager.changelogOffsets())
